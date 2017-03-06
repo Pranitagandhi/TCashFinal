2569a9743b8f268ac8ae794a3e163d0b3b9c81d7
Pranitagandhi <gandhipranita@gmail.com>
2017-03-06 14:03:24 +0530

    Update SignupController.java

--- src/com/tcash/Controller/SignupController.java
+++ src/com/tcash/Controller/SignupController.java
@@ -22,7 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.tcash.Bean.TcashBean;
 import com.tcash.Dao.TcashDao;
 import com.tcash.Dao.addmoneyDao;
@@ -33,6 +34,7 @@ public class SignupController {
 
 	@Autowired
 	TcashDao dao;
+	 private static Logger slf4jLogger = LoggerFactory.getLogger(SignupController.class);
 
 	@RequestMapping("/signUp")
 	public ModelAndView showform() {
@@ -42,9 +44,11 @@ public class SignupController {
 	@RequestMapping(value = "/save3", method = RequestMethod.POST)
 	public ModelAndView save3(@ModelAttribute("emp") TcashBean emp, HttpServletResponse response) throws IOException {
 		if (dao.save3(emp) == true) {
+
 			String message = "Mobile number is already Registered";
 			return new ModelAndView("signUp", "message", message);
 		} else {
+
 			dao.save(emp);
 			String message1 = "Registered successfully";
 			return new ModelAndView("Login", "message1", message1);
@@ -57,27 +61,28 @@ public class SignupController {
 		return new ModelAndView("Login", "command", new TcashBean());
 	}
 
-	@RequestMapping(value = "/save1", method = RequestMethod.POST)
+	@RequestMapping(value = "/save1", method = RequestMethod.GET)
 	public ModelAndView save1(@RequestParam("mobile_no") Long mobile_no, @ModelAttribute("emp") TcashBean emp) {
 		if (dao.save1(emp) == true) {
-			//Float b = dao.avlbal(mobile_no, emp);
+		   slf4jLogger.info("Mobile_no is registered, {}",mobile_no); 
 			ModelAndView modelAndView = new ModelAndView();
 			modelAndView.addObject("mobile_no", mobile_no);
-			modelAndView.setViewName("welcome");
-			//modelAndView.addObject("b", b);
+			modelAndView.setViewName("home");
+
 			return modelAndView;
 
 		} else {
+			slf4jLogger.error("Mobile_no is not registered ,{}",mobile_no);
 			String message = "Invalid username or password";
 			return new ModelAndView("Login", "message", message);
 		}
 
 	}
 
-	@RequestMapping(value = "welcome")
+	/*@RequestMapping(value = "welcome")
 	public ModelAndView showform2(@ModelAttribute("emp") TcashBean emp) {
 		return new ModelAndView("welcome", "command", new TcashBean());
-	}
+	}*/
 
 	@RequestMapping("/home")
 	public ModelAndView showform2() {
@@ -93,16 +98,19 @@ public class SignupController {
 	public ModelAndView save2(@ModelAttribute("mobile_no") Long mobile_no, @RequestParam("fname") String fname,
 			@RequestParam("lname") String lname, @RequestParam("email") String email,
 			@ModelAttribute("emp") TcashBean emp) {
+		
 		dao.save2(mobile_no, emp);
+		slf4jLogger.info("Profile is updated,{},{},{} for the user  {},",fname,lname,email,mobile_no);
+		System.out.println(emp.getUsername());
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("username", mobile_no);
+		modelAndView.addObject("mobile_no", mobile_no);
 		modelAndView.addObject("fname", fname);
 		modelAndView.addObject("lname", lname);
 		modelAndView.addObject("email", email);
 		modelAndView.setViewName("editoutput");
 
 		return modelAndView;
-
+		
 	}
 
 	@RequestMapping("/addmoney")
@@ -117,20 +125,22 @@ public class SignupController {
 		dao.add(mobile_no, emp);
 		if (dao.add1(mobile_no, emp) == true) {
 			dao.add3(mobile_no, emp);
+			slf4jLogger.info("Money {} updated successfully for the user ,{} ",balance,mobile_no);
 			Float b = dao.avlbal(mobile_no, emp);
 			ModelAndView modelAndView = new ModelAndView();
 			modelAndView.addObject("b", b);
 			modelAndView.addObject("balance", balance);
 			modelAndView.addObject("mobile_no", mobile_no);
 			String message = "Money added Succesfully !! Available balance " + b;
-			return new ModelAndView("home", "message", message);
+			return new ModelAndView("addmoney", "message", message);
 
 		} else {
 			dao.add2(mobile_no, emp);
+			slf4jLogger.info("Money {}  added successfully for the user ,{} ",balance,mobile_no);
 			ModelAndView modelAndView = new ModelAndView();
 			modelAndView.addObject("mobile_no", mobile_no);
 
-			modelAndView.setViewName("home");
+			modelAndView.setViewName("addmoney");
 
 			return modelAndView;
 		}
@@ -145,10 +155,12 @@ public class SignupController {
 	@RequestMapping(value = "/fundcheck", method = RequestMethod.POST)
 	public ModelAndView fundcheck(@ModelAttribute("mobile_no") Long mobile_no, @ModelAttribute("emp") TcashBean emp) {
 		if (dao.fundcheck(mobile_no, emp) == true) {
+			slf4jLogger.error("Recipient is registered");
 			if (dao.submit(mobile_no, emp) == 1) {
 				if (dao.fund(mobile_no, emp) == true) {
 					dao.fund3(mobile_no, emp);
 					dao.fund4(mobile_no, emp);
+					slf4jLogger.info("Fund transferred from {} to the recipient successfully",mobile_no);
 					Float b = dao.avlbal(mobile_no, emp);
 					ModelAndView modelAndView = new ModelAndView();
 					modelAndView.addObject("mobile_no", mobile_no);
@@ -160,6 +172,7 @@ public class SignupController {
 					dao.fund2(emp);
 					dao.fund3(mobile_no, emp);
 					dao.fund4(mobile_no, emp);
+					slf4jLogger.info("Fund transferred from {} to the recipient successfully",mobile_no);
 					Float b = dao.avlbal(mobile_no, emp);
 					ModelAndView modelAndView = new ModelAndView();
 					modelAndView.addObject("mobile_no", mobile_no);
@@ -169,21 +182,25 @@ public class SignupController {
 
 				}
 			} else {
+				slf4jLogger.error("Insufficient funds to transfer from user {}",mobile_no);
 				String message = "Insufficient Funds!";
 				return new ModelAndView("fundtransfer", "message", message);
 			}
 		} else {
+			slf4jLogger.error("Recipient is  not registered");
 			String message1 = "Recipient not registered!!!";
 			return new ModelAndView("fundtransfer", "message", message1);
 		}
 
 	}
 
-	@RequestMapping(value = "/retrieve2", method = RequestMethod.POST)
+	/*@RequestMapping(value = "/retrieve2", method = RequestMethod.POST)
 	public ModelAndView showform7(@ModelAttribute("mobile_no") Long mobile_no, @ModelAttribute("emp") TcashBean emp) {
 		java.util.List<TcashBean> list = dao.retrieve2(mobile_no, emp);
+		
 		return new ModelAndView("resultform", "list", list);
-	}
+	}*/
+
 	@RequestMapping(value = "/retrieve3", method = RequestMethod.POST)
 	public ModelAndView showform8(@ModelAttribute("mobile_no") Long mobile_no, @ModelAttribute("emp") TcashBean emp) {
 		java.util.List<TcashBean> list = dao.retrieve3(mobile_no, emp);
@@ -208,26 +225,9 @@ public class SignupController {
 	}
 
 	@RequestMapping("/logout")
-	public String logout(HttpSession session) {
+	public String logout(HttpSession session,@ModelAttribute("mobile_no") Long mobile_no) {
 		session.invalidate();
-		System.out.println("session logged out");
+		slf4jLogger.info("Session is logged out for mobile {}",mobile_no); 
 		return "index";
 	}
-	/*
-	 * @RequestMapping(value = "/image/{imageId}", method = RequestMethod.GET)
-	 * 
-	 * @ResponseBody public ModelAndView getImage(@PathVariable Long imageId) {
-	 * return new ModelAndView("imageId", "command", new TcashBean()); }
-	 *//*
-		 * @RequestMapping(value="",method = RequestMethod.POST) public
-		 * ModelAndView avlbal(@ModelAttribute("mobile_no") Long
-		 * mobile_no,@RequestParam("balance") float
-		 * balance, @ModelAttribute("emp") TcashBean emp) throws Exception {
-		 * 
-		 * 
-		 * Float b=dao.avlbal(mobile_no,emp); ModelAndView modelAndView = new
-		 * ModelAndView(); modelAndView.addObject("b", b);
-		 * modelAndView.setViewName("home"); return modelAndView; }
-		 */
 }
-
